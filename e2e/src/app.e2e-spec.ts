b46ade50ea6c640c67679536d38de06a20f9c4d3
angelrodgz <angelrodriguez@ucol.mx>
2019-10-14 08:42:27 -0500

    begin

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('my-angular-ap app is running!');
+    expect(page.getTitleText()).toEqual('Welcome to s2!');
   });
 
   afterEach(async () => {
